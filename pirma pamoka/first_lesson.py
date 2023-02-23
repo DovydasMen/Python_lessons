7882dffd73c26207370ce5b3ac21b759ffeb25a2
Dovydas Menkevičius <d.menkevicius@gmail.com>
2023-02-23 19:52:34 +0200

    Adding first lesson

---
+++ pirma pamoka/first_lesson.py
@@ -0,0 +1,14 @@
+print("Hello world")
+# outputas stringo informacija.
+a = "geras =mogus"
+b = a.center(20)
+# reikšmei a suteikiama reikšmė ( skaičiai šiuo atveju sveikieji INT, gali būti ir realūs, informacija bus float)
+print("Antanui metu:", b)
+# i6printina informaciją.
+
+
+txt = "Hello, welcome to my world."
+
+x = txt.find("e", 5, 10)
+
+print(x)
