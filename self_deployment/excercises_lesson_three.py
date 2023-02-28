e30f3d780ec52b9671b1b056f67c316e3aa5c0ff
Dovydas Menkevičius <d.menkevicius@gmail.com>
2023-02-28 15:35:29 +0200

    Adding ended lesson

--- self_deployment/excercises_lesson_three.py
+++ self_deployment/excercises_lesson_three.py
@@ -46,9 +46,57 @@ another_tuple[0] = 500
 Tuple is no modifyable !!!
 """
 
-my_list= [1, 5, 10 , 30]
-print(sum(my_list))
-print(my_list[0] * my_list[1] * my_list[2] * my_list[3])
-print(max(my_list))
+# my_list= [1, 5, 10 , 30]
+# # print(sum(my_list))
+# # print(my_list[0] * my_list[1] * my_list[2] * my_list[3])
+# # print(max(my_list))
+# # print(min(my_list))
+# # print(len(my_list))
 
+# # for item in my_list:
+# #     print(item)
+# #     print(item + 12)
+# # my_list[2] = 5
+# # print(my_list)
+# print(my_list[-1])
+# print(my_list[:2]) 
+# print(my_list[::2])
+# print(my_list[::-1])
+# print(my_list[0:2])
+
+my_list_two = [1, 2, 3]
+
+print(1 in my_list_two) #boool, parodo ar toks elementas yra
 #.....
+
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ TASK 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+Write a python program that sums up all items in the list (all items are integers or floats in list, create a list yourself)
+
+
+"""
+
+random_numbers_list = [1.1, 2, 44, 7, 8]
+sum_of_list = sum(random_numbers_list)
+print(sum_of_list)
+
+multiplication_of_list = random_numbers_list[0] * random_numbers_list[1] * random_numbers_list[2] * random_numbers_list[3] * random_numbers_list[4]
+print(multiplication_of_list)
+print(max(random_numbers_list))
+
+
+list_full_of_strings = ["Dukra", "Kita dukra", "Vyras", "Moteris", "Visi faini"]
+one_str_of_list = ''.join(list_full_of_strings)
+new_line_of_strings ='\n'.join(list_full_of_strings)
+print(one_str_of_list)
+print(new_line_of_strings)
+
+one_list = [1, 2.2, 3]
+two_list = [2, 4.4, 6]
+three_list = one_list + two_list
+
+print(three_list)
